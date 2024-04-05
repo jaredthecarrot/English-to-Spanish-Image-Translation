9fee39de80d420ca2623e7af220d20dbf6ba5c59
Cab00s38 <164224789+Cab00s38@users.noreply.github.com>
2024-04-05 11:16:14 -0500

    Add files via upload
    Attached is Pytesseract Application of Program
    Works to translate most image to text
    Requires download of Pytesseract and OpenCv Libraries and Programs to Machine to function
    PyTesseract: https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-5.3.3.20231005.exe
    OpenCV: https://opencv.org/releases/
    NOTES:
    Still working on applying live image detection and application to program. 
    Have to direct path Pytesseract and translated image to program to work
    PNG is the test file currently attached to program

---
+++ PyTesseract.py
@@ -0,0 +1,40 @@
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+def image_to_text(image_path):
+    try:
+        image = cv2.imread(image_path)
+        if image is None:
+            raise FileNotFoundError("Unable to read image file.")
+    except Exception as e:
+        print("Error loading image:", e)
+        return None
+    
+    try:
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    except Exception as e:
+        print("Error converting image to grayscale:", e)
+        return None
+    
+    try:
+        extracted_text = pytesseract.image_to_string(gray_image)
+        return extracted_text
+    except Exception as e:
+        print("Error extracting text using Tesseract OCR:", e)
+        return None
+
+
+image_path = r'C:\Users\ndela\Downloads\pytesseract-simple-python-optical-character-recognition-7.png'
+
+
+extracted_text = image_to_text(image_path)
+
+if extracted_text:
+    
+    print("Extracted Text:")
+    print(extracted_text)
+else:
+    print("Failed to extract text from the image.")
+
